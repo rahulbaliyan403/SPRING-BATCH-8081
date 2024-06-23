fed840fca2f0019ef1f7f7ff2408ccf59bb15cdd
rahulbaliyan403 <rahulbaliyan403@gmail.com>
2024-06-23 17:28:09 +0530

    Initial commit

---
+++ src/main/java/com/home/SpringBatch8081Application.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch8081Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch8081Application.class, args);
+	}
+
+}
